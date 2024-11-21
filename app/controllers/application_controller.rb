c414d64482b2964b7d2b4d3f89e7bbc1d584e78e
EC2 Default User <ec2-user@ip-172-31-18-117.eu-north-1.compute.internal>
2024-11-21 12:21:44 +0000

    [finish] アプリケーションの骨組み実装

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,20 @@
+class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    def after_sign_in_path_for(resource)
+        flash[:notice] = "User was successfully Loged in."
+        books_path
+    end
+
+    def after_sign_out_path_for(resource)
+        flash[:notice] = "User was successfully Loged out."
+        root_path
+    end
+
+    protected
+  
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :introduction])
+    end
+  end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,49 @@
+class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+  before_action :authenticate_user!, except: [:top]
+
+  def top
+    redirect_to root_path
+  end
+
+  def about
+    redirect_to home_about_path
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "User was successfully updated."
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to books_path
+    end
+  end
+
+end
